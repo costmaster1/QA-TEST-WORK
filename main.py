33dfc35c65a71a4acba250cbb413f1003a4b83bc
costmaster1 <osoben1@yandex.ru>
2023-03-24 16:38:16 +0500

    save 24.03.2023

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-from settings import maill, password, content_title, content_desc, tec_date
+from settings import maill, password, test
+from bs4 import BeautifulSoup
+import requests
+
 
 driver = webdriver.Chrome()
 chrome_options = Options()
@@ -73,57 +76,87 @@ try:
     # Подтверждаем кликабельность url для перехода в раздел  "Добавить новость" через явное ожидание...
     url_add_news = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='navbar-collapse']/ul/li[1]/a")))
 
-    # Кликаем по ссылке "Добавить новость"
+    # Кликаем по кнопке "Добавить новость"
     url_add_news.click()
+    time.sleep(10)
 
-    # Объявляем функцию обработки текущий даты для размещения новостей....
+    # Блок парсер...
+    # Создаем заголовок для отправки серверу...
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
 
-    def is_price_correct(driver, tec_date):
-        price_locator = (By.XPATH, '//input[@value="29.12.2022"] and contains(@input, "'+tec_date+'")]')
+    # Загружаем HTML-страницу с помощью библиотеки requests
+    url = "https://roo-nag.educhel.ru/government/news"
+    response = requests.get(url, headers=headers)
+    html = response.content
 
-        return is_price_correct(driver, tec_date)
+    # Инициализируем объект BeautifulSoup для парсинга HTML-кода
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Находим все блоки div вложенные в блок div c классом "eip-sortable-news news-masonry row"
+    divs = soup.find_all("div", class_="eip-sortable-news news-masonry row")
+
+    # Инициализируем переменную для хранения наибольшего значения id
+    max_id = None
+
+    # Проходимся по всем блокам div и ищем максимальное значение id
+    for div in divs:
+        div_id = div.get('id')
+        if div_id is not None and (max_id is None or int(div_id) > int(max_id)):
+            max_id = div_id
+
+
+
+    # Выводим наибольшее значение id на экран
+    print("Наибольший по значению id блок div: ", max_id)
+
+
+
+    # Подтверждаем кликабельность url для выхода из в режима редактирования...
+    url_add_news = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, f"//a[text()='{test}']"))).click
+    time.sleep(5)
 
 
 
 
 
 
-    # Отчищаем форму ввода news_carousel_header
-    # news_carousel_header.clear()
+    '''''
+    # Вводим название статьи...
+    title_news = driver.find_element(By.CSS_SELECTOR, "div[data-widget-id='w2']>div")
+    title_news.clear()
+    title_news.send_keys(content_title_news)
+    
+    # Вводим дату публикции статьи...
+    if content_date_news == 'none':
+        date_news = driver.find_element(By.CSS_SELECTOR, "div[data-widget-id='w3']>input")
+        date_news.clear()
+        date_news.send_keys(tec_date)
+    else:
+        date_news = driver.find_element(By.CSS_SELECTOR, "div[data-widget-id='w3']>input")
+        date_news.clear()
+        date_news.send_keys(content_date_news)
+     
 
+    # Вводим описание статьи...
+    desc_news = driver.find_element(By.CSS_SELECTOR, "div[data-widget-id='w4']>div")
+    desc_news.clear()
+    desc_news.send_keys(content_desc_news)
 
-    # Вводим Заголовок статьи
-    # news_carousel_header.send_keys(content_title)
+    # Подтверждаем кликабельность url для выхода из в режима редактирования...
+    url_add_news = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, "//a[text()='Выйти']"))).click
+    time.sleep(5)
 
-    # Подтверждаем вывод формы ввода news_carousel_desc через явное ожидание...
-    # news_carousel_desc = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='_api_eip_edit_EdudepPost_short']")))
-
-    # Отчищаем форму ввода news_carousel_desc
-    # news_carousel_desc.clear()
-
-    # Вводим Краткое описание статьи
-    # news_carousel_desc.send_keys(content_desc)
-
-    # Подтверждаем кликабельность url Сохранить через явное ожидание...
-    # url_save_news = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, "//a[text()='Сохранить']")))
-
-    # Кликаем по ссылке "Сохранить"
-    # url_save_news.click()
-
-    # Подтверждаем кликабельность url Продолжить чтение через явное ожидание...
-    # url_redactor_news = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='3147820']/article/a")))
-
-    # Кликаем по ссылке "Продолжить чтение"
-    # url_redactor_news.click()
-
-    # Засыпаем на 3 секунды
-    time.sleep(3)
+    '''''
 
 
 finally:
-    driver.close()                       # Прекращаем работу драйвера...
-    print("Тест пройден успешно!!!")     # выводим сообщение в консоль...
-    print(tec_date)                      # Выводим текущую дату
+    # Прекращаем работу драйвера...
+    driver.close()
+    print(divs)
+
+    # выводим сообщение в консоль...
+    print("Тест пройден успешно!!!")
+
     
 
 
